445ff29fba56be0cb7785021b1c5c4d45ea121a3
Lhizel Tabual <tabual.lhizel@auf.edu.ph>
2023-03-26 00:33:05 +0800

    finished

---
+++ hello.php
@@ -0,0 +1,14 @@
+<?php
+function greet() {
+    $message = "Hello";
+    $message .= " World!";
+
+
+    return $message;
+}
+
+
+echo greet();
+echo (" The date today is ");
+echo date("F j, Y, l");
+?>
